e511ffcded4b9a5a0fa1a9135f0fc32f348ec4a3
Javier Donoso <javier.donoso@nuilea.com>
2024-01-31 17:45:16 +0100

    fix

--- src/safe1Balance.ts
+++ src/safe1Balance.ts
@@ -13,7 +13,6 @@ import {
   RelayTransaction,
 } from "@safe-global/safe-core-sdk-types";
 
-
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
@@ -21,14 +20,12 @@ dotenv.config({ path: ".env" });
 import ContractInfo from "../ABI.json";
 
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
-let  RPC_URL = `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`;
-
+let RPC_URL = `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`;
 
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-
-const safeAddress =  "0xde80C1a5A01f9C8d81E13405d949eB165e1F4191";
+const safeAddress = "0xde80C1a5A01f9C8d81E13405d949eB165e1F4191";
 
 const targetAddress = ContractInfo.address;
 const GELATO_RELAY_API_KEY = process.env.GELATO_RELAY_API_KEY;
@@ -37,7 +34,7 @@ const nftContract = new ethers.Contract(
   ContractInfo.abi,
   signer
 );
-  console.log(signer.address)
+console.log(signer.address);
 const gasLimit = "8000000";
 
 async function relayTransaction() {
@@ -50,33 +47,26 @@ async function relayTransaction() {
     signerOrProvider: signer,
   });
 
-  const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapter })
+  const safeFactory = await SafeFactory.create({ ethAdapter: ethAdapter });
 
   const safeAccountConfig: SafeAccountConfig = {
-    owners: [
-      await signer.getAddress(),
-    ],
+    owners: [await signer.getAddress()],
     threshold: 1,
     // ... (Optional params)
-  }
-  
+  };
+
   /* This Safe is tied to owner 1 because the factory was initialized with
   an adapter that had owner 1 as the signer. */
-  console.log(68)
-    const safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig })
-    console.log(71)
-  const safeAddress = await safeSdkOwner1.getAddress()
 
- console.log(72,safeAddress)
+  const safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig });
+
+  const safeAddress = await safeSdkOwner1.getAddress();
 
   const safeSDK = await Safe.create({
     ethAdapter,
     safeAddress,
   });
 
-
-  
-  
   const safeTransactionData: MetaTransactionData = {
     to: targetAddress,
     data: nftContract.interface.encodeFunctionData("increment", []),
@@ -88,20 +78,16 @@ async function relayTransaction() {
     isSponsored: true,
   };
 
-
   const safeSingletonContract = await getSafeContract({
     ethAdapter: ethAdapter,
     safeVersion: await safeSDK.getContractVersion(),
   });
-
 
   // const standardizedSafeTx = await relayKit.createRelayedTransaction(
   //   {safe:safeSDK,
   //    transactions:[safeTransactionData],
   //   options}
   // );
-
-
 
   // const signedSafeTx = await safeSDK.signTransaction(standardizedSafeTx);
 
